3364b74a3b36d98fba9b51079d3b67974342cb90
d479 <daniel.madureira@ua.pt>
2024-03-26 23:56:53 +0000

    Created hero

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,64 +1,28 @@
-import 'package:closely_io/components/Drawer.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:closely_io/components/layout/Drawer.dart';
+import 'package:closely_io/components/layout/Hero.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final box = Hive.box('closely');
-    final user = box.get('user', defaultValue: '');
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
       drawer: const AppDrawer(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: const Column(
         children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Theme.of(context).colorScheme.primaryContainer,
-                  Theme.of(context).colorScheme.background,
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Text(
-                    'Welcome',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Text(
-                    user,
-                    style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.w600,
-                        color:
-                            Theme.of(context).colorScheme.onPrimaryContainer),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          AppHero(),
+          Text('Home Page')
+          
         ],
       ),
     );
